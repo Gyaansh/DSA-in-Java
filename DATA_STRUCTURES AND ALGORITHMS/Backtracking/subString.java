97a14808cea3da3c0d6281ac05ab2a7f05f0f123
Gyaansh <gyaansh70@gmail.com>
2026-03-14 20:04:48 +0530

    sub Sets of a string using backtracking

---
+++ DATA_STRUCTURES AND ALGORITHMS/Backtracking/subString.java
@@ -0,0 +1,24 @@
+class subString{
+
+        public static void findSubSet(String str, String ans, int i){
+            // base case
+            if(i == str.length()){
+                if(ans.length()==0)
+                    System.out.print("NULL");
+                else
+                    System.out.print(ans + " ");
+                return;
+            }
+            //choose to be in sub-set 
+            findSubSet(str, ans + str.charAt(i), i+1);
+            
+            //choose not to be in the sub-set
+            findSubSet(str, ans, i+1);
+        }
+
+
+    public static void main(String[] args) {
+        String str = "ABC";
+        findSubSet(str, "", 0);
+    }
+}
